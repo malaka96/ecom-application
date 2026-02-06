34449df4d11955c06c99095473d30dc35e749580
malaka96 <madhubhashana655@gmail.com>
2026-02-06 15:49:54 +0530

    created api to update user

--- src/main/java/edu/malaka96/ecom_application/controller/UserController.java
+++ src/main/java/edu/malaka96/ecom_application/controller/UserController.java
@@ -3,6 +3,8 @@ package edu.malaka96.ecom_application.controller;
 import edu.malaka96.ecom_application.model.User;
 import edu.malaka96.ecom_application.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,19 +17,31 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAll(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAll(){
+        //return ResponseEntity.ok(userService.getAllUsers());
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.I_AM_A_TEAPOT);
     }
 
     @GetMapping("/api/user/{id}")
-    public User getUser(@PathVariable String id){
-        return userService.getUser(id);
+    public ResponseEntity<User> getUser(@PathVariable String id){
+        return userService.getUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/api/adduser")
     public String addUser(@RequestBody User user){
         userService.addUser(user);
         return "User added successfully";
+    }
+
+    @PutMapping("/api/updateuser")
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        boolean updated = userService.updateUser(user);
+        if (updated)
+            return ResponseEntity.ok("User updated successfully" );
+        else
+            return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/edu/malaka96/ecom_application/service/UserService.java
+++ src/main/java/edu/malaka96/ecom_application/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,23 +13,34 @@ public class UserService {
     private List<User> usersList = new ArrayList<User>();
     private int addCount = 0;
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return usersList;
     }
 
-    public User getUser(String id){
-        for (User user : usersList){
-            if(user.getId().equals(id))
-                return user;
-        }
-        return null;
+    public Optional<User> getUser(String id) {
+        return usersList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
     }
 
-    public void addUser(User user){
+    public void addUser(User user) {
         user.setId(String.valueOf(addCount));
         usersList.add(user);
         addCount++;
     }
 
 
+    public Boolean updateUser(User updatedUser) {
+        return usersList.stream()
+                .filter(user -> user.getId().equals(updatedUser.getId()))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser
+                            .setEmail(updatedUser.getEmail());
+                    existingUser.setPassword(updatedUser.getPassword());
+                    return true;
+                })
+                .orElse(false);
+
+    }
 }
